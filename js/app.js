049077efb6a6d7d669698614f8f4e3beb178a37b
Val <valentin.dissais@gmail.com>
2017-01-16 15:11:46 +0100

    derniers ajouts

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
-var myApp = angular.module('GuessApp',[]);
+var myApp = angular.module('GuessApp', []);
 
 myApp.controller('GuessController', function($scope) {
-  let random = Math.floor(Math.random()*101);
-  console.log(random);
-  let turn_count = 0;
-  $scope.guessTheNumber= function(){
-      turn_count+=1;
-      if(turn_count>=7){
-        $scope.indication = "Game over!";
-      }
-      else if($scope.guess > random){
-        $scope.indication = "C'est moins!";
-      }
-      else if ($scope.guess < random) {
-        $scope.indication = "C'est plus!";
-      }
-      else if ($scope.guess === random) {
-        $scope.indication = "C'est gagné!";
-      }
-  };
+    let random = Math.floor(Math.random() * 100);
+    console.log(random);
+    $scope.turn_count = 7;
+    $scope.disable = false;
+    $scope.guessTheNumber = function() {
+        $scope.turn_count--;
+        if ($scope.turn_count === 0) {
+            $scope.indication = "Game over!";
+            $scope.disable = true;
+        } else if ($scope.guess > random) {
+            $scope.indication = "C'est moins!";
+        } else if ($scope.guess < random) {
+            $scope.indication = "C'est plus!";
+        } else if ($scope.guess === random) {
+            $scope.indication = "C'est gagné!";
+            $scope.disable = true;
+        }
+    };
+    let reset = document.getElementById('reset');
+    reset.addEventListener("click", function() {
+        window.location.reload();
+    });
 });
